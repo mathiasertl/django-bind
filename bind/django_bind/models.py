8202c33c29a0488216ef3d48dcb15a120f8aec04
Mathias Ertl <mati@er.tl>
2018-11-04 17:20:42 +0100

    start some basic models

--- bind/django_bind/models.py
+++ bind/django_bind/models.py
@@ -1,3 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-bind (https://github.com/mathiasertl/django-bind).
+#
+# django-bind is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-bind is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-bind.  If not,
+# see <http://www.gnu.org/licenses/>.
+
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now=True)
+    expires = models.DateTimeField(null=False, blank=False)
+
+    class Meta:
+        abstract = True
+
+
+class View(BaseModel):
+    name = models.CharField()
+
+
+class Zone(BaseModel):
+    created = models.DateTimeField(auto_now=True)
+    expires = models.DateTimeField(null=False, blank=False)
+
+    domain = models.CharField()
+    template = models.TextField()
